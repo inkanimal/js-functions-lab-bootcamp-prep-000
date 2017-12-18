5dc7d6264a1cf0c5a2bd256d9cb99ef533732b9b
geoffrey woodcock <theatomproject@gmail.com>
2017-12-18 19:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(){
+  console.log("Happy holidays!")
+}
+happyHolidays()
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+
+function happyHoliaysTo(holiday, name) {
+  return `${holiday}, ${name}!`
+}
+
+function holidayCountdown(numbeOfDays, holidayName){
+  return `It's ${numberOfDays} days until ${holidayName}!`
+}
+  
