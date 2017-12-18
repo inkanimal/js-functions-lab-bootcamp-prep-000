07c772b876fb05f30ed7c71ff148a9aa9f58b622
geoffrey woodcock <theatomproject@gmail.com>
2017-12-18 19:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function happyHolidayTo(holiday, name) {
 happyHolidaysTo("New Year", "Geoff")
 
 function holidayCountdown(holiday, days) {
-  return `It\'s ${days} days until ${holiday}!"`
+  return `It\'s ${days} days until ${holiday}!`
 }
 holidayCountdown("Christmas" "7")
